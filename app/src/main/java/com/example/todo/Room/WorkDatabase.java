7475b7cc700abc79dd370435f32081692b7f9f51
Jannatul Mahiya Mutasha <74957251+jannatulmahiyamutasha@users.noreply.github.com>
2020-12-19 17:04:21 +0600

    Update WorkDatabase.java
    .

--- app/src/main/java/com/example/todo/Room/WorkDatabase.java
+++ app/src/main/java/com/example/todo/Room/WorkDatabase.java
@@ -21,40 +21,12 @@ public abstract class WorkDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     WorkDatabase.class, "work_database")
                     .fallbackToDestructiveMigration()
-                   // .addCallback(roomcallback)
+                
                     .build();
 
         }
 
         return instance;
     }
-
-//    private static RoomDatabase.Callback roomcallback = new RoomDatabase.Callback() {
-//        @Override
-//        public void onCreate(@NonNull SupportSQLiteDatabase db) {
-//            super.onCreate(db);
-//            new PopulateDbAsynTask(instance).execute();
-//
-//        }
-//    };
-//
-//    private static class PopulateDbAsynTask extends AsyncTask<Void, Void, Void> {
-//        private WorkDao workDao;
-//
-//        private PopulateDbAsynTask(WorkDatabase database) {
-//            workDao = database.workDao();
-//        }
-//
-//        @Override
-//        protected Void doInBackground(Void... voids) {
-//
-//            workDao.insert(new Work("final project Work", "9/4/2020", "4:27 PM", 3,true));
-//            workDao.insert(new Work("think about Trigonous", "10/4/2020", "3:40 PM", 4,false));
-//            workDao.insert(new Work("think about Job", "9/1/2020", "10:00 PM", 2,true));
-//            workDao.insert(new Work("think about Life", "10/4/2020", "3:45 AM", 5,false));
-//
-//            return null;
-//        }
-//    }
 
 }
